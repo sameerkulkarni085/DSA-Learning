37912c5cb7b8a61b8c9293cbee381df354b73a29
Sameer <you@example.com>
2025-01-22 11:33:45 +0530

    1/22 Appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,16 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './HomePage';
 import Array from './components/ArrayPage/index';
 import LinkedList from "./components/LinkedList/index";
-import StringComponent from "./components/Strings/index";
-import StackComponent from "./components/Stack/index"; // Ensure correct import
+import Searching from "./components/Searching/index";
+import Sorting from "./components/Sorting/index";
+import Recursion from "./components/Recursion/index";
+import Backtracking from "./components/Backtracking/index";
+import Greedy from "./components/Greedy/index";
+import Dynamic from "./components/Dynamic/index";
+import Pattern from "./components/Pattern/index";
+import Divide from "./components/Divide/index";
+import Branch from "./components/Branch/index";
+import Random from "./components/Random/index";
 
 const App = () => {
   return (
@@ -13,8 +21,16 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/array" element={<Array />} />
         <Route path="/linkedlist" element={<LinkedList />} />
-        <Route path="/string" element={<StringComponent />} />
-        <Route path="/stack" element={<StackComponent />} /> {/* Add route for stack page */}
+        <Route path="/searching" element={<Searching />} />
+        <Route path="/sorting" element={<Sorting />} />
+        <Route path="/recursion" element={<Recursion />} />
+        <Route path="/backtracking" element={<Backtracking />} />
+        <Route path="/greedy" element={<Greedy />} />
+        <Route path="/dynamic" element={<Dynamic />} />
+        <Route path="/pattern" element={<Pattern />} />
+        <Route path="/divide" element={<Divide />} />
+        <Route path="/branch" element={<Branch />} />
+        <Route path="/random" element={<Random />} />
       </Routes>
     </Router>
   );
