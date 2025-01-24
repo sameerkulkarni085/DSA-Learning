dbc8d093e6ac8f1a37fab2daffc81c1060a5ee40
Sameer <you@example.com>
2025-01-24 11:12:45 +0530

    1/24 vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/DSA-Learning/', // Replace with your repository name
+  base: '', // Replace with your repository name
+  build: {
+    chunkSizeWarningLimit: 1000, // Adjust the limit as needed (default is 500 KB)
+  },
 });
