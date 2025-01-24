21860703eba211c20b4d1c04a6e326d5b8bbab6b
Sameer <you@example.com>
2025-01-24 11:15:48 +0530

    24/1 vite changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '', // Replace with your repository name
+  base: '/', // Set to '/' for root path or '/repository-name/' for GitHub Pages
   build: {
     chunkSizeWarningLimit: 1000, // Adjust the limit as needed (default is 500 KB)
   },
