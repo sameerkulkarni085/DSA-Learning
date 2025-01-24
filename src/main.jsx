20ad97fd513da8f373de14b4c938fb7b4150d925
Amruth <you@example.com>
2025-01-24 13:24:14 +0530

    24/01/25 - trial

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+// Ensure you have a root element in your HTML file with id 'root'
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
