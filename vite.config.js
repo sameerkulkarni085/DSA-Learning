fa934e11fe77c0512d7a62f00e4536d77165f42e
Sameer <you@example.com>
2025-01-24 11:26:04 +0530

    24/1 push change

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Set to '/' for root path or '/repository-name/' for GitHub Pages
+  base: '', // Set to '/' for root path or '/repository-name/' for GitHub Pages
   build: {
     chunkSizeWarningLimit: 1000, // Adjust the limit as needed (default is 500 KB)
   },
